29688e736295429d6978a4406a3a07d9f725a1de
Elisa <elisat15@uw.edu>
2019-11-04 09:21:13 -0800

    remove all code for setting up ajax demo

--- ajax-demo/ajax-demo.js
+++ ajax-demo/ajax-demo.js
@@ -1,46 +1,42 @@
 'use strict';
 
 // http://www.omdbapi.com/ documentation
-const API_URL = "http://www.omdbapi.com/?apikey=" + API_KEY + "&t=";
 
-let searchBtn = document.querySelector("#search");
-searchBtn.addEventListener("click", function(e) {
-    e.preventDefault();
 
-});
+// // Uncomment after completing fetch function
 
-function fetchMovie(movie) {
+// function handleError(err) {
+//     let parent = document.querySelector("#results");
+//     let errorMsg = document.createElement("p");
+//     errorMsg.classList = "alert alert-danger";
+//     errorMsg.textContent = err;
+//     parent.append(errorMsg);
+// }
 
-}
+// function generateContent(data) {
+//     let parent = document.querySelector("#results");
+//     appendPoster(data.Poster, parent);
+//     appendDetails(data, parent);
+// }
 
-function handleError(err) {
-    let parent = document.querySelector("#results");
-    let errorMsg = document.createElement("p");
-    errorMsg.classList = "alert alert-danger";
-    errorMsg.textContent = err;
-    parent.append(errorMsg);
-}
+// function appendDetails(data, parent) {
+//     appendDetail("Title", data.Title, parent);
+//     appendDetail("Director", data.Director, parent);
+//     appendDetail("Genre", data.Genre, parent);
+//     appendDetail("Language", data.Language, parent);
+//     appendDetail("Released", data.Language, parent);
+//     appendDetail("Rating", data.imdbRating, parent);
+// }
 
-function generateContent(data) {
-    let parent = document.querySelector("#results");
-    appendPoster(data.Poster, parent);
-    appendDetails("Title", data.Title, parent);
-    appendDetails("Director", data.Director, parent);
-    appendDetails("Genre", data.Genre, parent);
-    appendDetails("Language", data.Language, parent);
-    appendDetails("Released", data.Language, parent);
-    appendDetails("Rating", data.imdbRating, parent);
-}
+// function appendDetail(type, info, parent) {
+//     let detail = document.createElement("p");
+//     detail.textContent = type + ": " + info;
+//     parent.append(detail);
+// }
 
-function appendDetails(type, info, parent) {
-    let detail = document.createElement("p");
-    detail.textContent = type + ": " + info;
-    parent.append(detail);
-}
-
-function appendPoster(location, parent) {
-    let poster = document.createElement("img");
-    poster.src = location;
-    poster.classList = "poster";
-    parent.append(poster);
-}
+// function appendPoster(location, parent) {
+//     let poster = document.createElement("img");
+//     poster.src = location;
+//     poster.classList = "poster";
+//     parent.append(poster);
+// }
